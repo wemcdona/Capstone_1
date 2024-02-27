b2d80d45cc405037ee11028fdb353ed1f410e990
Walter McDonald <wemcdona@gmail.com>
2024-02-27 13:42:12 -0600

    style1

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class UserAddForm(FlaskForm):
@@ -9,6 +9,7 @@ class UserAddForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
     image_url = StringField('(Optional) Image URL')
+    submit = SubmitField('Add User')
 
 class UserEditForm(FlaskForm):
     """Form for editing users."""
@@ -17,6 +18,7 @@ class UserEditForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     image_url = StringField('(Optional) Image URL')
     password = PasswordField('Password', validators=[Length(min=6)])
+    submit = SubmitField('Update Profile')
 
 class LoginForm(FlaskForm):
     """Login form."""
