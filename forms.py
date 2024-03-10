793fc304298089c5ef25570aaab2bc6120d0bd89
Walter McDonald <wemcdona@gmail.com>
2024-03-09 18:02:53 -0600

    style changes

--- forms.py
+++ forms.py
@@ -1,20 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, InputRequired, Email, Length
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='Please enter a valid email address')])
     password = PasswordField('Password', validators=[Length(min=6)])
     image_url = StringField('(Optional) Image URL')
 
 class UserEditForm(FlaskForm):
     """Form for editing users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='Please enter a vallid email address')])
     image_url = StringField('(Optional) Image URL')
     password = PasswordField('Password', validators=[Length(min=6)])
     submit = SubmitField('Update Profile')
@@ -22,5 +22,5 @@ class UserEditForm(FlaskForm):
 class LoginForm(FlaskForm):
     """Login form."""
 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
